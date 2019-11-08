28e99c89e2d905c9bb1291c4f58eb6a49f8407ea
ckrapu <ckrapu@gmail.com>
2019-11-07 19:43:20 -0500

    Added install_requires fields and description for PyPi

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 from distutils.core import setup
 from setuptools import setup, find_packages
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='sprcom',
-      version='1.0',
+      version='0.1',
       description='Bayesian spatial regression of communities',
       author='Christopher Krapu',
       author_email='ckrapu@gmail.com',
-      packages=find_packages()
-     )
+      packages=find_packages(),
+      download_url='https://github.com/ckrapu/sprcom/archive/v0.1-beta.tar.gz',
+      install_requires=['scipy','numpy','pymc3','theano','matplotlib','seaborn'],
+      long_description=long_description,
+      long_description_content_type='text/markdown')
